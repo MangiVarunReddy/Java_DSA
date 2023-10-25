721f0dd4bbabf8d8207438f591522f33987180ec
MangiVarunReddy <mangivarunreddy@gmail.com>
2023-10-25 09:57:50 -0500

    renamed folders

---
+++ Month_1/Week_1/Day_1/Pattern1.java
@@ -0,0 +1,23 @@
+package Month_1.Week_1.Day_1;
+
+/**
+ * * * * *
+ * * * * *
+ * * * * *
+ * * * * *
+ * * * * *
+ * 
+ */
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
