4a0bc5b530b6502caf2e57c6cadfc6ddfe472b69
MangiVarunReddy <mangivarunreddy@gmail.com>
2023-10-25 09:20:06 -0500

    Update Pattern6.java

--- Month_1/Week_1/Day_2/Pattern6.java
+++ Month_1/Week_1/Day_2/Pattern6.java
@@ -15,30 +15,30 @@ package Month_1.Week_1.Day_2;
 
 public class Pattern6 {
     public static void main(String[] args) {
-        int n=5;
+        int n = 5;
 
-        for(int i=1;i<=2*n;i++){
+        for (int i = 1; i <= 2 * n; i++) {
 
-            if(i<=n){
+            if (i <= n) {
 
-            
-            for(int j=1;j<=n - i;j++){
-                System.out.print(" ");
+                for (int j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            } else {
+                for (int k = 1; k <= i - n - 1; k++) {
+                    System.out.print(" ");
+                }
+                for (int q = 1; q <= 2 * n - (2 * (i - n) - 1); q++) {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            for(int k=1;k<=2*i-1;k++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        else{
-            for(int k=1;k<=i - n - 1;k++){
-                System.out.print(" ");
-            }
-            for(int q=1;q<=2 * n - (2 * (i - n) - 1);q++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
         }
     }
 }
+// This the end of Pattern Programs. If have good a good Knowledge in writing
+// loops.
