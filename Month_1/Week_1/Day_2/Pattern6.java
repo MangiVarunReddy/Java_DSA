c02565ea494eb64bf529d7b1d7ebcd25266672ff
MangiVarunReddy <mangivarunreddy@gmail.com>
2023-10-24 22:24:27 -0500

    Pattern 6

---
+++ Month_1/Week_1/Day_2/Pattern6.java
@@ -0,0 +1,31 @@
+package Month_1.Week_1.Day_2;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        int n=5;
+
+        for(int i=1;i<=2*n;i++){
+
+            if(i<=n){
+
+            
+            for(int j=1;j<=n - i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        else{
+            for(int k=1;k<=i - n - 1;k++){
+                System.out.print(" ");
+            }
+            for(int q=1;q<=2 * n - (2 * (i - n) - 1);q++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        }
+    }
+}
