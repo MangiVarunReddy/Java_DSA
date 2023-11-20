50c04d47d4c25182551fa9f3f71909b02a638353
MangiVarunReddy <mangivarunreddy@gmail.com>
2023-11-19 23:12:11 -0600

    first week3 day1 program

--- Month_1/Week_2/Day_7/LeetCode189.java
+++ Month_1/Week_2/Day_7/LeetCode189.java
@@ -4,9 +4,9 @@ public class LeetCode189 {
 
     public static void main(String[] args) {
 
-        int arr[] = { -1, -100, 3, 99 };
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
         int n = arr.length;
-        int k = 2;
+        int k = 3;
         int j = 0;
         int x;
         while (j < k) {
